96c44bf0c266485013d8117815e9673664342479
Marla Albuquerque <marla@albuque.com>
2024-09-04 16:35:52 -0300

    feat(doom/org): minor config changes

--- modules/shared/doom/org.el
+++ modules/shared/doom/org.el
@@ -1,5 +1,9 @@
 ;;; $DOOMDIR/org.el -*- lexical-binding: t; -*-
 
+(after! org-roam-mode
+  (setq org-roam-dailies-directory "~/notes/org/roam/dailies/"
+	org-roam-directory "~/notes/org/roam"))
+
 (after! org
   (setq org-log-into-drawer t
 	org-log-done-with-time t
@@ -9,14 +13,36 @@
 	org-log-redeadline 'time
 	org-log-reschedule 'time
 	org-insert-heading-respect-content nil
-	org-agenda-files "~/notes/org/"
+	org-agenda-files (list "~/notes/org")
 	org-startup-align-all-tables t
 	org-auto-align-tags nil
 	org-tags-column 0
 	org-fold-catch-invisible-edits 'show-and-error
 	org-hide-emphasis-markers t
 	org-pretty-entities t
-	org-startup-folded "fold"))
+	org-startup-folded "fold"
+	org-tag-alist
+	'(;; places
+	  ("@home" . ?h)
+	  ("@out" . ?o)
+
+	  ;; devices
+	  ("@computer" . ?c)
+	  ("@iphone" . ?i)
+	  ("@android" . ?a)
+	  ("@tower" . ?t)
+	  ("@keyboard" . ?k)
+
+	  ;; activities/hobbies
+	  ("#programming" . ?p)
+	  ("#music" . ?m)
+	  ("#photography" . ?g)
+
+	  ;; alters
+	  ("!switch" . ?S)
+	  ("!nova" . ?N)
+	  ("!persona" . ?P))))
+
 (add-hook! org-mode
 	   :remove #'org-superstar-mode
 	   :append
@@ -26,6 +52,14 @@
 	   'org-modern-mode
 	   'org-modern-agenda
 	   '+org-pretty-mode)
+
+(add-hook! org-agenda-mode-hook
+  (setq org-agenda-custom-commands
+	'(("d" "daily" ((agenda "" ((org-agenda-span 'day)
+				    (org-deadline-warning-days 2)))))
+	  ("u" "untagged tasks" tags-todo "-{.*=}" ((org-agenda-overriding-header "Untagged")))
+	  ("k" "before leaving @home" tags-todo "+@out" ((org-agenda-overriding-header "checks for before leaving the house"))))))
+
 ;; ledger mode + (org-mode + org-bable)
 (after! ledger-mode
   (map! :map ledger-mode-map
