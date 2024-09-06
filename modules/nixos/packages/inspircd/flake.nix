a70dd4d5d9b7abc4b74898b8ec98c688a0a9b559
Marla Albuquerque <marla@albuque.com>
2024-09-05 21:13:26 -0300

    feat(packages): inspircd

---
+++ modules/nixos/packages/inspircd/flake.nix
@@ -0,0 +1,49 @@
+
+{
+  description = "InspIRCd v4.2.0";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs  }:
+    let system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system;  };
+        lib = pkgs.lib;
+        in
+          {
+            packages.${system}.inspircd = pkgs.stenv.mkDerivation rec {
+              pname = "inspircd";
+              version = "4.2.0";
+
+              src = pkgs.fetchFromGitHub {
+                owner = "inspircd";
+                repo = "inspircd";
+                rev = "v${version}";
+                sha256 = "0q79bsdmnqqwi6hn2wfwk2zwwkqjqblwr5sx370ah2kv7qmdz7ls";
+              };
+
+              buildInput = with pkgs; [ cmake openssl libcap ];
+
+              cmakeFlags = [
+                "-DCMAKE_INSTALL_PREFIX=$out"
+                "-DINSPIRC_ENABLE_OPENSSL=ON"
+                "-DINSPIRCD_ENABLE_EXAMPLES=OFF"
+                "-DINSPIRCD_STATIC=OFF"
+              ];
+
+              postPatch = ''
+              '';
+
+              preConfigure = "mkdir build && cd build";
+              configurePhase = "cmake .. $cmakeFlags";
+              buildPhase = "make";
+              installPhase = "make install DESTDIR=$out";
+
+              meta = with lib; {
+                description = "A modular, libweight IRC Daemon";
+                homepage = "https://inspircd.org/";
+                license = licenses.gnl2Plus;
+                maintainers = with maintainers; [ marlalain ];
+              };
+            };
+          };
+}
